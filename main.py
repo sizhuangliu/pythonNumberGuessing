fcac622d3a5fd504e6ed0a37072aac5c288020b0
sizhuangliu <42162239+sizhuangliu@users.noreply.github.com>
2021-04-03 14:18:15 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+
+class NumberCompare:
+    '''
+    Number Guessing game is finished.'''
+
+    def __init__(self, num, randnum):
+        self.num = num
+        self.randnum = randnum
+
+    def guess_num(self):
+
+        if self.num == "g":
+            print('you are closing, the guess number is ' + str(self.randnum))
+            return False
+        elif self.num.isnumeric():
+            if int(self.num) == self.randnum:
+                print('Congratulations!!!!')
+                return False
+            elif int(self.num) < self.randnum:
+                print('Sorry, too small')
+                return True
+            else:
+                print('Sorry, too big')
+                return True
+        else:
+            print("Invalid input, please type a number or 'G' if you want to give up")
+            return True
+
+
+number_to_guess = random.randint(1, 100)
+guess = input('Enter an integer between 1 to 100 : ')
+
+p = NumberCompare(guess, number_to_guess)
+
+while p.guess_num():
+    p.num = input('Enter an integer between 1 to 100 : ')
+
+print(NumberCompare.__doc__)
